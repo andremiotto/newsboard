7ebf9e44735ac3598b572563e26a715edf247354
andre miotto <miotto082@gmail.com>
2018-05-10 11:34:12 -0300

    Articles controller created. Articles can be created recording its owner_id

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
 
-  has_many :reviews
-  has_many :users, through: :reviews
+  # has_many :reviews
+  # has_many :users, through: :reviews
+
+  belongs_to :user, class_name: 'User', foreign_key: 'owner_id'
 end
--- db/migrate/20180507191547_create_articles.rb
+++ db/migrate/20180507191547_create_articles.rb
@@ -3,6 +3,7 @@ class CreateArticles < ActiveRecord::Migration[5.1]
     create_table :articles do |t|
       t.text :url
       t.text :comment
+      t.integer :owner_id
 
       t.timestamps
     end
