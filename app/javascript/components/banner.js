ecb5dfc87db594763a8d084d22e9b1d14712be02
andre miotto <miotto082@gmail.com>
2018-05-10 16:07:46 -0300

    Banner added, JS typing added, logo added

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -3,9 +3,11 @@ import Typed from 'typed.js';
 
 function loadDynamicBannerText() {
   new Typed('#banner-typed-text', {
-    strings: ["Change your life", "Learn to code"],
-    typeSpeed: 50,
-    loop: true
+    strings: ["Share News", "Share Knowledge"],
+    startDelay: 2000,
+    typeSpeed: 100,
+    backDelay: 1000,
+    backSpeed: 110,
   });
 }
 
